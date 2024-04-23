98d7c8055b7c24f7c148cb5d487d8a6f6a22f293
Antonio <antonio.ibarra99@outlook.es>
2024-04-22 18:03:24 -0700

    Administrador de productos v1.1

--- src/Views/Products.tsx
+++ src/Views/Products.tsx
@@ -10,40 +10,49 @@ export async function loader() {
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
-  console.log(data);
-  return {};
+  return data;
 }
 
 export default function Products() {
   const products = useLoaderData() as Product[];
   return (
     <>
-      <div className="flex justify-between">
-        <h2 className="text-4xl font-black text-slate-500">Productos</h2>
-        <Link
-          to="/productos/nuevos"
-          className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
-        >
-          Agregar producto
-        </Link>
-      </div>
-      <div className="p-2">
-        <table className="w-full mt-5 table-auto">
-          <thead className="bg-slate-800 text-white">
-            <tr>
-              <th className="p-2">Producto</th>
-              <th className="p-2">Precio</th>
-              <th className="p-2">Disponibilidad</th>
-              <th className="p-2">Acciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <ProductDetails key={product._id} product={product} />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {products ? (
+        <div>
+          <div className="flex justify-between">
+            <h2 className="text-4xl font-black text-slate-500">Productos</h2>
+            <Link
+              to="/productos/nuevos"
+              className="rounded-md bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+            >
+              Agregar producto
+            </Link>
+          </div>
+          <div className="p-2">
+            <table className="w-full mt-5 table-auto">
+              <thead className="bg-slate-800 text-white">
+                <tr>
+                  <th className="p-2">Producto</th>
+                  <th className="p-2">Precio</th>
+                  <th className="p-2">Disponibilidad</th>
+                  <th className="p-2">Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {products.map((product) => (
+                  <ProductDetails key={product._id} product={product} />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center items-center">
+          <p className="font-black text-slate-500">
+            No hay información para mostrar ó la API no esta funcionando
+          </p>
+        </div>
+      )}
     </>
   );
 }
